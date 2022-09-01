783e54eb8478d13e76b678611e5291a8da701631
syabyr <mybays@sina.com>
2022-09-02 00:49:15 +0800

    esb

---
+++ nRF5_SDK_12.3.0_d7731ad/myproject/proprietary_rf/esb_low_power_ptx/_build/nrf51822_xxaa_sdk_errors.c.d
@@ -0,0 +1,23 @@
+_build/nrf51822_xxaa_sdk_errors.c.o: \
+ ../../../components/libraries/util/sdk_errors.c \
+ ../../../components/libraries/util/sdk_errors.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/stdint.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/features.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/_newlib_version.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/_intsup.h \
+ /usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/_stdint.h \
+ ../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h sdk_config.h \
+ ../../../components/libraries/util/nordic_common.h
+../../../components/libraries/util/sdk_errors.h:
+/usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h:
+/usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/stdint.h:
+/usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/machine/_default_types.h:
+/usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/features.h:
+/usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/_newlib_version.h:
+/usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/_intsup.h:
+/usr/local/Cellar/arm-none-eabi-gcc/10.3-2021.07/gcc/arm-none-eabi/include/sys/_stdint.h:
+../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h:
+sdk_config.h:
+../../../components/libraries/util/nordic_common.h:
