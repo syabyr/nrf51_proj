78900e9e78b8e24e2ccbc97126f09201e47721b9
syabyr <mybays@sina.com>
2025-04-03 09:42:11 +0800

    bl600

---
+++ pio-bl600-examples/arduino-blink/src/Blink.cpp
@@ -0,0 +1,31 @@
+/*
+ * Blink
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+
+#include <Arduino.h>
+
+//BLUE:18
+//RED:19
+#define LED_BUILTIN 13
+#define LED_BLUE 18
+#define LED_RED 19
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(LED_BLUE, OUTPUT);
+}
+
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BLUE, HIGH);
+  // wait for a second
+  delay(1000);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BLUE, LOW);
+   // wait for a second
+  delay(1000);
+}
